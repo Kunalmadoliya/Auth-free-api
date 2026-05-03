586adccd979f1da313eeeb5dbe7b6108eb00434e
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-05-03 20:10:38 +0530

    done

--- Auth-project/src/components/GetUser.tsx
+++ Auth-project/src/components/GetUser.tsx
@@ -1,37 +1,37 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-const GetUser = () => {
-  const [user, setUser] = useState<any>(null);
+// const GetUser = () => {
+//   const [user, setUser] = useState<any>(null);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const res = await fetch(
-          "https://api.freeapi.app/api/v1/users/current-user",
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
+//   useEffect(() => {
+//     const fetchUser = async () => {
+//       try {
+//         const res = await fetch(
+//           "https://api.freeapi.app/api/v1/users/current-user",
+//           {
+//             method: "GET",
+//             headers: {
+//               "Content-Type": "application/json",
+//             },
+//           }
+//         );
 
-        const data = await res.json();
-        console.log(data);
-        setUser(data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
+//         const data = await res.json();
+//         console.log(data);
+//         setUser(data);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     };
 
-    fetchUser();
-  }, []);
+//     fetchUser();
+//   }, []);
 
-  return (
-    <>
-      <h1>hello</h1>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <h1>hello</h1>
+//     </>
+//   );
+// };
 
-export default GetUser;
+// export default GetUser;
